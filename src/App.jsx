3e8e083a1fdcdc4ed340ec2a9cd18d908b8149e3
Prithvi1666 <prithvirajdaud143@gmail.com>
2026-09-25 14:08:17 +0530

    Add About, Stats, and Dishes sections; enhance Navbar scroll effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './sections/HeroSection'
+import About from './sections/About'
+import Stats from './sections/Stats'
+import Dishes from './sections/Dishes'
+
 
 const App = () => {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <About />
+      <Stats />
+      <Dishes />
     </>
   )
 }
