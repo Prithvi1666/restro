178adba1324d1aad9a4364702f28617216ea9b95
Prithvi1666 <prithvirajdaud143@gmail.com>
2026-09-25 15:33:58 +0530

    Add Features section to showcase unique dining experiences

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HeroSection from './sections/HeroSection'
 import About from './sections/About'
 import Stats from './sections/Stats'
 import Dishes from './sections/Dishes'
+import Features from './sections/Features'
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <About />
       <Stats />
       <Dishes />
+      <Features />
     </>
   )
 }
